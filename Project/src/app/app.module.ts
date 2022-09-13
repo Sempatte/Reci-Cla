5383319ec05ae897a256026e277904d030a38d3e
Sebastian De la Torre <sempatte@protonmail.com>
2022-09-13 15:38:43 -0500

    Listar scores fixed

--- Project/src/app/app.module.ts
+++ Project/src/app/app.module.ts
@@ -5,7 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RecicladorComponent } from './page/reciclador/reciclador.component';
-import{MatTableModule} from '@angular/material/table';
+import { MatTableModule} from '@angular/material/table';
+import { MatIconModule} from '@angular/material/icon';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './page/home/home.component';
 import { RecolectorComponent } from './page/recolector/recolector.component';
@@ -26,7 +27,8 @@ import { ListarScoresRecolectorComponent } from './page/recolector/listar-scores
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatTableModule
+    MatTableModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
