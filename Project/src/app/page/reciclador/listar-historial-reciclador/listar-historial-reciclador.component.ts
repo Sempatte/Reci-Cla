b6bda8e286144ddea10110c72bab712bae5dce76
Sebastian De la Torre <sempatte@protonmail.com>
2022-09-14 01:10:14 -0500

    Styles

--- Project/src/app/page/reciclador/listar-historial-reciclador/listar-historial-reciclador.component.ts
+++ Project/src/app/page/reciclador/listar-historial-reciclador/listar-historial-reciclador.component.ts
@@ -11,6 +11,8 @@ export class ListarHistorialRecicladorComponent implements OnInit {
 
   error : string = "";
   nombreReciclador : string = "";
+  data_apellidoReciclador : string = "";
+  data_nombreReciclador : string = ""
   dataSource: MatTableDataSource<any> = new MatTableDataSource();
   _find : boolean = false;
   displayedColumns: string[] = ['busquedaID','fecha' ,'hora' ,'contenido'];
@@ -22,11 +24,13 @@ export class ListarHistorialRecicladorComponent implements OnInit {
   }
 
   buscarHistorial(){
-
+    this.nombreReciclador = this.nombreReciclador.charAt(0).toUpperCase() + this.nombreReciclador.slice(1);
     this.uS.getHistorialXReciclador(this.nombreReciclador).subscribe(
+      
       (data) => {
-
         if (data.length > 0) {
+          this.data_apellidoReciclador = data[0].apellido;
+          this.data_nombreReciclador = data[0].nombre;
           data = data[0].history[0].busquedas;
           this.dataSource = new MatTableDataSource(data);
           this._find = true;
@@ -36,6 +40,10 @@ export class ListarHistorialRecicladorComponent implements OnInit {
           this.error = "No se encontraron resultados";
         }
 
+      }, 
+      (error) => {
+        this._find = false;
+        this.error = "Uknown error";
       }
     )
   }
