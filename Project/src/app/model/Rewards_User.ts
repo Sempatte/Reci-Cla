87336ba02f949e4adf41b9bc188fee2ffae28cd2
GiancarloMilon <87823313+GiancarloMilon@users.noreply.github.com>
2022-09-16 13:38:52 -0500

    Reward_User

---
+++ Project/src/app/model/Rewards_User.ts
@@ -0,0 +1,9 @@
+import { Reward } from './Reward';
+import { User } from './User';
+
+export class Rewards_Users {
+    Reward : Reward = new Reward();
+    User : User = new User();
+    Description: String = "";
+    Claimed: boolean = true;;
+  }
