b27319396c019bbc57f90e3fe7be9f44c4ef7a36
Sebastian De la Torre <sempatte@protonmail.com>
2022-09-25 16:51:37 -0500

    Insertar y modificar usuarios (añadido)

---
+++ Project/src/app/utils/utils.js
@@ -0,0 +1,11 @@
+export default function validarFechaMenorActual(date){
+    var x=new Date();
+    var fecha = date.split("/");
+    x.setFullYear(fecha[2],fecha[1]-1,fecha[0]);
+    var today = new Date();
+
+    if (x >= today)
+      return false;
+    else
+      return true;
+}
