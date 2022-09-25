99077882e4791ee647fafaae069c1432e42d765e
Sebastian De la Torre <sempatte@protonmail.com>
2022-09-25 14:20:35 -0500

    Products folder deleted

--- Project/src/app/page/ubication/ubication.component.ts
+++ Project/src/app/page/ubication/ubication.component.ts
@@ -12,15 +12,8 @@ export class UbicationComponent implements OnInit {
 
   DataSource: MatTableDataSource<Ubication> = new MatTableDataSource();
   listaUbication: Ubication[] = [];
-<<<<<<< HEAD
+
   displayedColumns: string[] = ['idUbication','Distrito', 'Direccion' ];
-=======
-<<<<<<< Updated upstream
-  displayedColumns: string[] = ['idUbication','Distrito', 'Direccion' ];
-=======
-  displayedColumns: string[] = ['idUbication','Distrito', 'Direccion', 'acciones' ];
->>>>>>> Stashed changes
->>>>>>> parent of 50ecc1f (e)
 
   constructor(private ubS: UbicacionService) { }
 
