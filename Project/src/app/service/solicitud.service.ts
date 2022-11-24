41d16a17b89ab55d60b92e2be71d3b51195d8605
Dacf18 <u201810646@upc.edu.pe>
2022-11-24 11:52:58 -0500

    service solicitud

---
+++ Project/src/app/service/solicitud.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Solicitud } from '../model/Solitituc';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SolicitudService {
+  private url:string=`${environment.host}/solicitudes`;
+  private listaCambio=new Subject<Solicitud[]>()
+  private confirmaEliminacion=new Subject<Boolean>()
+  constructor(private http:HttpClient) { }
+  listarSolicitud(){
+    return this.http.get<Solicitud[]>(this.url);
+  }
+  insertarSolicitud(soli:Solicitud){
+    return this.http.post(this.url,soli);
+  }
+  modifySolicitud(soli:Solicitud){
+    return this.http.put(this.url,soli);
+  }
+  eliminarSolicitud(id:number){
+    return this.http.delete(`${this.url}/${id}`);
+  }
+  buscar(texto:string){
+    return this.http.post<Solicitud[]>(`${this.url}/buscar`,texto);
+  }
+  ListarIdSolicitud(id:number){
+    return this.http.get<Solicitud>(`${this.url}/${id}`);
+  }
+  getListaSolicitud(){
+    return this.listaCambio.asObservable();
+  }
+  setListaSolicitud(listaNueva:Solicitud[]){
+    this.listaCambio.next(listaNueva);
+  }
+  getConfirmaEliminacion(){
+    return this.confirmaEliminacion.asObservable();
+  }
+  setConfirmaEliminacion(estado:Boolean){
+    this.confirmaEliminacion.next(estado);
+  }
+}
